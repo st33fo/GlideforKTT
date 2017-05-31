237519e193d3c236901ab447e4baf5cd2b114244
Stefan <swp5ab@virginia.edu>
2017-05-31 17:27:39 -0400

    Fixed the extra spacing between the tabs and the profile info.

--- app/src/main/java/com/st33fo/glideforktt/tabs/SlidingTabLayout.java
+++ app/src/main/java/com/st33fo/glideforktt/tabs/SlidingTabLayout.java
@@ -207,7 +207,7 @@ public class SlidingTabLayout extends HorizontalScrollView {
 
             if (mDistributeEvenly) {
                 LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) tabView.getLayoutParams();
-                lp.width = 0;
+              //  lp.width = 0;
                 lp.weight = 1;
             }
 
